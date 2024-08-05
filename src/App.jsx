f45310453ab1633c5cb7be0ebd86eecf5ac8f1f8
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-08-05 18:21:37 +0600

    initialized this repository with a react application, added tailwind css to it

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen px-8 bg-lime-200">
+        <div className='text-center pt-12 text-4xl md:text-6xl font-semibold'>Todo List</div>
+      </div>
+    </>
+  )
+}
+
+export default App
